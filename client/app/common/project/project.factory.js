8fe4fa40c5900a771c1b2a987be3c7d20ff4c30e
Marc-Olivier Castagnetti <marcolivier.castagnetti@gmail.com>
2016-04-19 16:37:08 +0200

    change list style

--- client/app/common/project/project.factory.js
+++ client/app/common/project/project.factory.js
@@ -2,12 +2,12 @@ let ProjectFactory = function () {
 	var factory = {};
 
 	var projectList =[
-		{"id" : 1, "name" : "Portfolio Project", "image" : "http://lorempicsum.com/nemo/350/200/1", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
-		{"id" : 2, "name" : "Lorem", "image" : "http://lorempicsum.com/nemo/350/200/2", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
-		{"id" : 3, "name" : "Ipsum", "image" : "http://lorempicsum.com/nemo/350/200/3", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
-		{"id" : 4, "name" : "Dolor", "image" : "http://lorempicsum.com/nemo/350/200/4", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
-		{"id" : 5, "name" : "Si Amet", "image" : "http://lorempicsum.com/nemo/350/200/5", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
-		{"id" : 6, "name" : "Tebaibi", "image" : "http://lorempicsum.com/nemo/350/200/6", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."}
+		{"id" : 1, "title" : "Portfolio Project", "image" : "http://lorempicsum.com/up/627/300/1", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
+		{"id" : 2, "title" : "Lorem", "image" : "http://lorempicsum.com/up/627/300/2", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
+		{"id" : 3, "title" : "Ipsum", "image" : "http://lorempicsum.com/up/627/300/3", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
+		{"id" : 4, "title" : "Dolor", "image" : "http://lorempicsum.com/up/627/300/4", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
+		{"id" : 5, "title" : "Si Amet", "image" : "http://lorempicsum.com/up/627/300/5", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."},
+		{"id" : 6, "title" : "Tebaibi", "image" : "http://lorempicsum.com/up/627/300/6", "short_description" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, itaque nesciunt!."}
 	] ;
 
 	factory.getList = () => {
